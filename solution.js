3012ad7ff64a6bb314e3789a79af9af4ecbbf785
gayoungyeom <gayoungyeom@gmail.com>
2023-03-16 01:17:43 +0900

    자바스크립트 fs 입력 템플릿

---
+++ solution.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+let input = fs.readFileSync('../tc.txt').toString().trim().split('\n');
+
+let [n, ...arr] = input;
+
+function solution(n, arr) {
+  let ans = 0;
+  return ans;
+}
+
+console.log(solution(n, arr));

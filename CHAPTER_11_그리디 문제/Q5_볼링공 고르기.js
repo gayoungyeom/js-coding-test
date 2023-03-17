5f56dc93626d37c0af38b126ae552a1e1dcb23bd
gayoung <gayoungyeom@gmail.com>
2023-03-17 12:48:43 +0900

    add: 볼링공 고르기

---
+++ CHAPTER_11_그리디 문제/Q5_볼링공 고르기.js
@@ -0,0 +1,31 @@
+// 5 3
+// 1 3 2 3 2
+
+const fs = require('fs');
+let input = fs.readFileSync('../tc.txt').toString().trim().split('\n');
+
+let [nm, arr] = input;
+const [n, m] = nm.split(' ').map((v) => +v);
+arr = arr.split(' ').map((v) => +v);
+
+function solution(n, arr) {
+  const nC2 = (n) => {
+    return (n * (n - 1)) / 2;
+  };
+
+  let weight = [...Array(11).fill(0)]; //무게별 볼링공 개수
+  for (const w of arr) {
+    weight[w]++;
+  }
+
+  let dup = 0;
+  for (let i = 1; i <= m; i++) {
+    if (weight[i] > 1) {
+      dup += nC2(weight[i]);
+    }
+  }
+
+  return nC2(n) - dup;
+}
+
+console.log(solution(n, arr));

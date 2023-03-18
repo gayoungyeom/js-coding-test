28eca457656b5d8289259f1bea52962d8a463da2
gayoung <gayoungyeom@gmail.com>
2023-03-19 01:10:33 +0900

    add: 럭키 스트레이트

---
+++ CHAPTER_12_구현 문제/Q7_럭키 스트레이트.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+let input = fs.readFileSync('../tc.txt').toString().trim();
+
+let point = input.split('').map((v) => +v);
+
+function solution(point) {
+  const n = point.length;
+  const left = point.slice(0, n / 2).reduce((a, b) => a + b);
+  const right = point.slice(n / 2, n).reduce((a, b) => a + b);
+  return left === right ? 'LUCKY' : 'READY';
+}
+
+console.log(solution(point));

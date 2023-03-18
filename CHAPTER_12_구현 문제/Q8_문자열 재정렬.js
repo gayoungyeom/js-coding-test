75ccd1e1b6a2346dad3b9d77d8e80e53a26a6c38
gayoung <gayoungyeom@gmail.com>
2023-03-19 01:22:43 +0900

    add: 문자열 재정렬

---
+++ CHAPTER_12_구현 문제/Q8_문자열 재정렬.js
@@ -0,0 +1,25 @@
+//AJKDLSI412K4JSJ9D
+
+const fs = require('fs');
+let input = fs.readFileSync('../tc.txt').toString().trim();
+
+let str = input;
+
+function solution(str) {
+  let result = [];
+  let sum = 0;
+  for (const s of str) {
+    if (isNaN(s)) {
+      result.push(s);
+    } else {
+      sum += +s;
+    }
+  }
+
+  result.sort();
+  if (sum) result.push(sum);
+
+  return result.join('');
+}
+
+console.log(solution(str));

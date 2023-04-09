1d8cc1840441d0f66f55619b47fd57f5ba2e2c54
gayoung <gayoungyeom@gmail.com>
2023-04-09 17:31:05 +0900

    refact: 치킨 배달

--- CHAPTER_12_구현 문제/Q13_치킨 배달.js
+++ CHAPTER_12_구현 문제/Q13_치킨 배달.js
@@ -1,68 +1,51 @@
-//fs 모듈을 사용할 경우 백준 런타임 에러(ENOENT) 발생
-const readline = require('readline');
+const fs = require('fs');
+let input = fs.readFileSync('./tc.txt').toString().trim().split('\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+let [nm, ...arr] = input;
+const [n, m] = nm.split(' ').map(Number);
+arr = arr.map((str) => str.split(' ').map(Number));
 
-let input = [];
-
-rl.on('line', function (line) {
-  input.push(line);
-}).on('close', function () {
-  let [nm, ...arr] = input;
-  const [n, m] = nm.split(' ').map((v) => +v);
-  arr = arr.map((str) => str.split(' ').map((v) => +v));
-
-  function solution(n, m, arr) {
-    const houses = [];
-    const chickens = [];
-    for (let i = 0; i < n; i++) {
-      for (let j = 0; j < n; j++) {
-        if (arr[i][j] === 1) {
-          houses.push([i + 1, j + 1]);
-        }
-        if (arr[i][j] === 2) {
-          chickens.push([i + 1, j + 1]);
-        }
+function solution(n, m, arr) {
+  const chickens = [];
+  const houses = [];
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (arr[i][j] === 1) {
+        houses.push([i + 1, j + 1]);
+      } else if (arr[i][j] === 2) {
+        chickens.push([i + 1, j + 1]);
       }
     }
-
-    let ans = Infinity;
-    const nCr = (n, cur, tmp) => {
-      if (tmp.length === m) {
-        //m개의 치킨집 선택 후 치킨 거리 계산
-        const dist = [];
-        for (const house of houses) {
-          const [r1, c1] = house;
-          let min = Infinity;
-          for (const i of tmp) {
-            const [r2, c2] = chickens[i];
-            min = Math.min(min, Math.abs(r1 - r2) + Math.abs(c1 - c2));
-          }
-          dist.push(min);
-        }
-        ans = Math.min(
-          ans,
-          dist.reduce((a, b) => a + b)
-        );
-        return;
-      }
-
-      for (let i = cur; i < n; i++) {
-        tmp.push(i);
-        nCr(n, i + 1, tmp);
-        tmp.pop();
-      }
-    };
-
-    nCr(chickens.length, 0, []);
-
-    return ans;
   }
 
-  console.log(solution(n, m, arr));
+  let ans = Infinity;
 
-  process.exit();
-});
+  const nCr = (cur, tmp) => {
+    if (tmp.length === m) {
+      let sum = 0;
+      for (let i = 0; i < houses.length; i++) {
+        const [r1, c1] = houses[i];
+        let min = Infinity;
+        for (let j = 0; j < m; j++) {
+          const [r2, c2] = chickens[tmp[j]];
+          min = Math.min(min, Math.abs(r1 - r2) + Math.abs(c1 - c2));
+        }
+        sum += min;
+      }
+      ans = Math.min(ans, sum);
+      return;
+    }
+
+    for (let i = cur; i < chickens.length; i++) {
+      tmp.push(i);
+      nCr(i + 1, tmp);
+      tmp.pop();
+    }
+  };
+
+  nCr(0, []);
+
+  return ans;
+}
+
+console.log(solution(n, m, arr));

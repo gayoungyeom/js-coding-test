9c8df686f2b68e4c3943cb5f8fe576e4462f5d56
gayoung <gayoungyeom@gmail.com>
2023-04-11 16:13:32 +0900

    refact: 정렬된 배열에서 특정 수의 개수 구하기

--- CHAPTER_15_이진탐색 문제/Q27_정렬된 배열에서 특정 수의 개수 구하기.js
+++ CHAPTER_15_이진탐색 문제/Q27_정렬된 배열에서 특정 수의 개수 구하기.js
@@ -2,9 +2,10 @@ const fs = require('fs');
 let input = fs.readFileSync('../tc.txt').toString().trim().split('\n');
 
 let [nx, arr] = input;
-const [n, x] = nx.split(' ').map((v) => +v);
-arr = arr.split(' ').map((v) => +v);
+const [n, x] = nx.split(' ').map(Number);
+arr = arr.split(' ').map(Number);
 
+//#1
 function solution(n, x, arr) {
   let min = Infinity; //x가 등장하는 첫 번째 인덱스
   let max = -Infinity; //x가 등장하는 마지막 인덱스
@@ -28,6 +29,23 @@ function solution(n, x, arr) {
   binarySearch(0, n - 1, x);
 
   return flag ? max - min + 1 : -1;
+}
+
+//#2
+function solution2(n, m, arr) {
+  const set = new Set();
+  const binarySearch = (start, end, target) => {
+    if (start > end) return -1;
+
+    const mid = ~~((start + end) / 2);
+    if (arr[mid] === target) set.add(mid);
+    binarySearch(start, mid - 1, target);
+    binarySearch(mid + 1, end, target);
+  };
+
+  binarySearch(0, n, m);
+
+  return set.size || -1;
 }
 
 console.log(solution(n, x, arr));
